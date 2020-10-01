52adbc0d13ed1f936dad035cf15f3707e5d46168
Deepali Jain <61299175+deej17@users.noreply.github.com>
2020-09-30 22:45:57 -0400

    Update Projects.js

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -3,25 +3,35 @@ export const projects = {
     quote: "Lots of code! Lots of fun stuff!",
     projectList: [
         {
-            name: "Project 1: GetMed",
+            name: "Project 1: Portfolio Builder",
+            description: "Create and cusotmize a portfolio in a jiffy for free.",
+            link: "https://portfoliobuilder-prjt3-rtg.herokuapp.com/",
+            Technologies: "React, MongDB"
+        }, {
+            name: "Project 2: GetMed",
             description: "Get medicine from your preferred pharmacy at your doorstep as soon as want.",
-            link: "https://damp-badlands-68384.herokuapp.com/"
+            link: "https://damp-badlands-68384.herokuapp.com/",
+            Technologies: "Javascript, MySql, APIs"
         }, {
-            name: "Project 2: Note Taker",
+            name: "Project 3: Note Taker",
             description: "Enables user to write, save, and delete notes.",
-            link: "https://notetaker-dj.herokuapp.com/"
+            link: "https://notetaker-dj.herokuapp.com/",
+            Technologies: "Javascript, MySql"
         }, {
-            name: "Project 3: Music Venue Planner",
+            name: "Project 4: Music Venue Planner",
             description: "Search music events and concerts in the New York City and plan your next event.",
-            link: "https://deej17.github.io/deepali-homework7/home.html"
+            link: "https://deej17.github.io/deepali-homework7/home.html",
+            Technologies: "HTMl, CSS, APIs"
         }, {
-            name: "Project 4: Password Generator",
+            name: "Project 5: Password Generator",
             description: "Generate a secure password randomaly that meets a certain criteria.",
-            link: "https://deej17.github.io/deepali-homework3/"
+            link: "https://deej17.github.io/deepali-homework3/",
+            Technologies: "Javascript"
         }, {
-            name: "Project 5: Workday Scheduler",
+            name: "Project 6: Workday Scheduler",
             description: "Plan workday day and simplify the workflow.",
-            link: "https://deej17.github.io/deej17-homework5/"
+            link: "https://deej17.github.io/deej17-homework5/",
+            Technologies: "Javascript, IndexedDB"
         }
     ]
 }
